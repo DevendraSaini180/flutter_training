39fee9bfb85beea1f53f38c1af6c79cd21222666
DevendraSaini180 <devensaini1@gmail.com>
2022-09-14 09:37:09 +0530

    sep 12 task use of Image Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_training/pages/login.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +16,65 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future captureImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to capture image: $e');
+    }
+  }
+
+  Widget _setImageView() {
+    if (image != null) {
+      return Image.file(image!, width: 500, height: 500);
+    } else {
+      return Text("Please select an image");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: "Image Picker Lirary",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Practice App"),
+          title: const Text("Image Picker Example"),
         ),
-        body: SafeArea(
-          child: const Login(),
+        body: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text("Pick Image from Gallery"),
+                  onPressed: () {
+                    pickImage();
+                  }),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text("Pick Image from Camera"),
+                  onPressed: () {
+                    captureImage();
+                  }),
+              _setImageView(),
+            ],
+          ),
         ),
       ),
     );
